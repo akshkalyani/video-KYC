b9f3ce1fe6c6517258b9e133e376deede7d821ed
aksh <akshkalyani@gmail.com>
2024-02-22 12:02:23 +0530

    chenges on AK end

--- route/basicRoute.js
+++
@@ -1,8 +0,0 @@
-const express = require("express");
-const router = express();
-
-const userController = require("../controllers/userController");
-
-router.get("/", userController.loadIndex);
-
-module.exports = router;
